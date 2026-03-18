6854f993538521b71bde0d4eac8c2764d7ad081f
kaushikmak <kaushikmak35@gmail.com>
2026-03-19 01:08:10 +0530

    [fix] empty comment bug

--- app/schemas/comments.ts
+++ app/schemas/comments.ts
@@ -2,6 +2,6 @@ import { Id } from "@/convex/_generated/dataModel";
 import z from "zod";
 
 export const commentSchema = z.object({
-    body: z.string(),
+    body: z.string().min(4,"Comment must have 4 letters"),
     postId: z.custom<Id<"posts">>()
 });
